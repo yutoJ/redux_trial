59994b222d2e8d8c78bf8d12eddff14ac44d0356
yutoJ <yutoogata4@gmail.com>
2017-09-30 01:51:24 +0000

    implement react router

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,15 +1,27 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Switch,
+} from 'react-router-dom';
 
 import SearchPage from './SearchPage';
+import AboutPage from './AboutPage';
 
 const App = () => (
-  <div className="app">
-    <ul className="left-navi">
-      <li><a href="/">Hotel Search</a></li>
-      <li><a href="/">About</a></li>
-    </ul>
-    <SearchPage />
-  </div>
-)
+  <Router>
+    <div className="app">
+      <ul className="left-navi">
+        <li><Link to="/">Hotel Search</Link></li>
+        <li><Link to="/about">About</Link></li>
+      </ul>
+      <Switch>
+        <Route exact path="/" component={SearchPage} />
+        <Route exact path="/about" component={AboutPage} />
+      </Switch>
+    </div>
+  </Router>
+);
 
 export default App;
